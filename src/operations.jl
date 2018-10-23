8fef6fb6d3cf3f4e97b54b5c6a391ddd71255f7e
Hessam Mehr <hessammehr@gmail.com>
2018-10-23 17:01:45 +0100

    Simplify signature of `distance`.

--- src/operations.jl
+++ src/operations.jl
@@ -16,22 +16,22 @@ on the modelling each spectrum's peaks two-dimensional Gaussian in the
 mass-intensity plane and subtracting the two. The square of the resulting
 difference signal has a nice closed-form integral over the mass-intensity plane.
 """
-function distance(ms1::MassSpectrum{T}, ms2::MassSpectrum{T}, σmass, σintensity) where T
+function distance(ms1::MassSpectrum, ms2::MassSpectrum, σmass, σintensity)
     mass = [ms1.mass; ms2.mass]
     intensity = [ms1.intensity; ms2.intensity]
     α = [ms1.intensity; -1 .* ms2.intensity]
-    d = zero(T)
-    max_d = zero(T)
+    d = 0.0
+    max_d = 0.0
     for i in eachindex(mass)
         for j in eachindex(mass)
             mass_contrib = -(mass[i] - mass[j])^2 / (4σmass^2)
             intensity_contrib = -(intensity[i] - intensity[j])^2 / (4σintensity^2)
             term = α[i] * α[j] * exp(mass_contrib + intensity_contrib)
             d += term
-            if term > zero(T)
+            if term > 0.0
                 max_d += term
             end
         end
     end
-    max(zero(T), d) / max_d
+    max(0.0, d) / max_d
 end
